5cd092231d63fed13c730d64b9bc6f6c2401ab4a
liangfengli <915098904@qq.com>
2018-06-11 20:56:03 +0800

    添加 bill

---
+++ src/Smart.API.Adapter.BizCore/JD/JDBillBLL.cs
@@ -0,0 +1,38 @@
+﻿using Smart.API.Adapter.DataAccess.Core.JD;
+using Smart.API.Adapter.Models.Core.JD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart.API.Adapter.BizCore.JD
+{
+    public class JDBillBLL
+    {
+        JDBillDAL dal = new JDBillDAL();
+
+        /// <summary>
+        /// 插入JD账单数据
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public bool Insert(JDBillModel model)
+        {
+            model.QrCode = string.IsNullOrWhiteSpace(model.QrCode) ? "" : model.QrCode;
+            model.Cost = string.IsNullOrWhiteSpace(model.Cost) ? "" : model.Cost;
+            model.CreatTime = DateTime.Now;
+            return dal.Insert<JDBillModel>(model);
+        }
+
+        /// <summary>
+        /// 根据LogNo获取JD账单
+        /// </summary>
+        /// <param name="sLogNo"></param>
+        /// <returns></returns>
+        public JDBillModel GetJDBillByLogNo(string sLogNo)
+        {
+            return dal.GetJDBillByLogNo(sLogNo);
+        }
+    }
+}
---
+++ src/Smart.API.Adapter.BizCore/JD/ParkWhiteListBLL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Smart.API.Adapter.Models;
+using Smart.API.Adapter.DataAccess.Core.JD;
+
+
+namespace Smart.API.Adapter.BizCore.JD
+{
+    public class ParkWhiteListBLL
+    {
+        ParkWhiteListDAL dal = new ParkWhiteListDAL();
+        public ICollection<VehicleInfo> GetParkWhiteList()
+        {
+            return dal.FindAll<VehicleInfo>();
+        }
+    }
+}
