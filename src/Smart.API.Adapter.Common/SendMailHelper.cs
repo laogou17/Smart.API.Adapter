68286ed87aeb7410c43bb4663297d724ae17d58a
liangfengli <915098904@qq.com>
2018-06-14 20:57:11 +0800

    邮件可以发送多人

--- src/Smart.API.Adapter.Common/SendMailHelper.cs
+++ src/Smart.API.Adapter.Common/SendMailHelper.cs
@@ -12,7 +12,7 @@ namespace Smart.API.Adapter.Common
     public class SendMailHelper
     {
         public SendMailHelper()
-        { 
+        {
         }
 
         /// <summary>
@@ -29,56 +29,77 @@ namespace Smart.API.Adapter.Common
         /// 发送邮件
         /// </summary>
         /// <param name="isAsync">是否异步发送</param>
-        public void SendMail(bool isAsync=true)
+        public void SendMail(bool isAsync = true)
         {
-            MailHelper mail = new MailHelper(isAsync);
-            mail.From = CommonSettings.EmailUserName;
-            mail.AddReceive(EmailAddrType.To, CommonSettings.EmailTo,"");
+            try
+            {
+                MailHelper mail = new MailHelper(isAsync);
+                mail.From = CommonSettings.EmailUserName;
 
-            if (string.IsNullOrWhiteSpace(Subject))
-            {
-                Subject = "这是个标题";
-            }
-            mail.Subject = Subject;
-            if (string.IsNullOrWhiteSpace(Body))
-            {
-                Body = "这是邮件内容";
-            }
-            
-            // Guid.NewGuid() 防止重复内容，被SMTP服务器拒绝接收邮件
-            mail.Body = Body +"_"+ Guid.NewGuid();
-            mail.IsBodyHtml = true;
+                string sEmailTo = CommonSettings.EmailTo;
 
-            if (!mail.ExistsSmtpClient())
-            {
-                SmtpClient client = new SmtpHelper(CommonSettings.EmailSMTP, CommonSettings.EmailPort, CommonSettings.EmailSSL, CommonSettings.EmailUserName, CommonSettings.EmailPassword).SmtpClient;
-                mail.AsycUserState = "邮件[" + Subject + "]";
-                client.SendCompleted += (send, args) =>
+                if (sEmailTo.Contains(";"))
                 {
-                    AsyncCompletedEventArgs arg = args;
+                    string[] EmailToArray = sEmailTo.Split(';');
+                    foreach (string item in EmailToArray)
+                    {
+                        mail.AddReceive(EmailAddrType.To, item, "");
+                    }
+                }
+                else
+                {
+                    mail.AddReceive(EmailAddrType.To, CommonSettings.EmailTo, "");
+                }
+                if (string.IsNullOrWhiteSpace(Subject))
+                {
+                    Subject = "这是个标题";
+                }
+                mail.Subject = Subject;
+                if (string.IsNullOrWhiteSpace(Body))
+                {
+                    Body = "这是邮件内容";
+                }
 
-                    if (arg.Error == null)
+                // Guid.NewGuid() 防止重复内容，被SMTP服务器拒绝接收邮件
+                mail.Body = Body + "_" + Guid.NewGuid();
+                mail.IsBodyHtml = true;
+
+                if (!mail.ExistsSmtpClient())
+                {
+                    SmtpClient client = new SmtpHelper(CommonSettings.EmailSMTP, CommonSettings.EmailPort, CommonSettings.EmailSSL, CommonSettings.EmailUserName, CommonSettings.EmailPassword).SmtpClient;
+                    mail.AsycUserState = "邮件[" + Subject + "]";
+                    client.SendCompleted += (send, args) =>
                     {
-                        // 需要注意的事使用 MailHelper 发送异步邮件，其UserState是 MailUserState 类型
-                        LogHelper.Info("邮件日志："+((MailUserState)args.UserState).UserState.ToString() + "已发送完成.");
-                    }
-                    else
-                    {
-                         LogHelper.Error(String.Format("{0} 异常：{1}"
-                            , ((MailUserState)args.UserState).UserState.ToString() + "发送失败."
-                            , (arg.Error.InnerException == null ? arg.Error.Message : arg.Error.Message + arg.Error.InnerException.Message)));
-                        // 标识异常已处理，否则若有异常，会抛出异常
-                        ((MailUserState)args.UserState).IsErrorHandle = true;
-                    }
-                };
-                mail.SetSmtpClient(client, true);
+                        AsyncCompletedEventArgs arg = args;
+
+                        if (arg.Error == null)
+                        {
+                            // 需要注意的事使用 MailHelper 发送异步邮件，其UserState是 MailUserState 类型
+                            LogHelper.Info("邮件日志：" + ((MailUserState)args.UserState).UserState.ToString() + "已发送完成.");
+                        }
+                        else
+                        {
+                            LogHelper.Error(String.Format("{0} 异常：{1}"
+                               , ((MailUserState)args.UserState).UserState.ToString() + "发送失败."
+                               , (arg.Error.InnerException == null ? arg.Error.Message : arg.Error.Message + arg.Error.InnerException.Message)));
+                            // 标识异常已处理，否则若有异常，会抛出异常
+                            ((MailUserState)args.UserState).IsErrorHandle = true;
+                        }
+                    };
+                    mail.SetSmtpClient(client, true);
+                }
+                else
+                {
+                    mail.AsycUserState = "邮件[" + Subject + "]邮件已发送完成。";
+                }
+                mail.SendOneMail();
+                mail.Reset();
             }
-            else
+            catch (Exception ex)
             {
-                mail.AsycUserState = "邮件[" + Subject + "]邮件已发送完成。";
+
+                LogHelper.Error("发送邮件错误", ex);
             }
-            mail.SendOneMail();
-            mail.Reset();
         }
     }
 }
